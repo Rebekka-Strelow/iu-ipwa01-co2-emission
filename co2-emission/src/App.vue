45a6a7ed1422e12f1b9d9956b324533cd9385df5
Rebekka Strelow <Rebekka.Strelow@gmx.de>
2022-12-03 15:00:45 +0100

    CSS-Framework
    ElementPlus hinzugefügt

--- co2-emission/src/App.vue
+++ co2-emission/src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="common-layout">
+    <el-container class="full_height">
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+  },
+  data: () => ({
+    test: "hello"
+  })
 }
 </script>
 
 <style>
+html,body{
+ height:100vh;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.full_height{
+  height:100vh;
+}
+
 </style>
