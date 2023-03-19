2def965cabbf22794e2a62cc976101cc7e032d46
Rebekka Strelow <Rebekka.Strelow@gmx.de>
2023-03-19 14:12:01 +0100

    Datenschutz und Impressum

--- co2-emission-frontend/src/components/ImpressumContent.vue
+++ co2-emission-frontend/src/components/ImpressumContent.vue
@@ -1,5 +1,25 @@
 <template>
+    <div>
     <h1>Impressum</h1>
+    <h3>Angaben gemäß § 5 TMG</h3>
+        <p>Max Muster<br/>
+        Musterweg<br/>
+        12345 Musterstadt</p>
+
+    <h3>Vertreten durch:</h3>
+    <p>Max Muster</p>
+
+<h3>Kontakt:</h3>
+<p><strong>Telefon</strong>: 01234-789456<br/>
+<strong>Fax</strong>: 1234-56789<br/>
+<strong>E-Mail</strong>: max@muster.de</p>
+
+<h3>Umsatzsteuer-ID:</h3>
+<p>Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz: Musterustid.</p>
+
+<h3>Wirtschafts-ID:</h3>
+<p>Musterwirtschaftsid</p>
+</div>
 </template>
 
 <script>
