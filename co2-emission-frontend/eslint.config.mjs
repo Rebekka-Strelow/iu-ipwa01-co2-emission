cce3f86e4fdcaf4ddb0fea2ba9166e015c184a79
Rebekka Strelow <Rebekka.Strelow@gmx.de>
2025-04-14 20:11:55 +0200

    Versionskonflikt gefixt

--- co2-emission-frontend/eslint.config.mjs
+++ co2-emission-frontend/eslint.config.mjs
@@ -3,7 +3,7 @@ import globals from 'globals'
 
 export default [
   // add more generic rulesets here, such as:
-  // js.configs.recommended,
+  //js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
   // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
   {
